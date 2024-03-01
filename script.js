81075b5ea5a76032d4297dc358a72224185782c2
Hermxn <hermanhrynko@gmail.com>
2024-03-01 18:29:38 +0100

    mouseover, mouseout affects; assets added

--- script.js
+++ script.js
@@ -7,60 +7,87 @@ function main () {
         const card = createCard(sectionCards);
         fillCard(card, pokemon);
         styleCard(card);
+        card.mainContainer.addEventListener("mouseover", hooverBright);
+        card.mainContainer.addEventListener("mouseout", hooverDark);
     }
 }
 
 function createCard (sectionCards) {
-    const container = document.createElement("div");
-    const image = document.createElement("img");
-    const index = document.createElement("p");
-    const name = document.createElement("h3");
-    const type = document.createElement("p");
-    sectionCards.appendChild(container);
-    container.appendChild(image);
-    container.appendChild(index);
-    container.appendChild(name);
-    container.appendChild(type);
+    const mainContainer = document.createElement("div");
+    const wrapperImage = document.createElement("div");
+    const wrapperDescription = document.createElement("div");
+    const imageElement = document.createElement("img");
+    const indexElement = document.createElement("p");
+    const nameElement = document.createElement("h3");
+    const typeElement = document.createElement("p");
+    sectionCards.appendChild(mainContainer);
+    mainContainer.appendChild(wrapperImage);
+    mainContainer.appendChild(wrapperDescription);
+    wrapperImage.appendChild(imageElement);
+    wrapperDescription.appendChild(indexElement);
+    wrapperDescription.appendChild(nameElement);
+    wrapperDescription.appendChild(typeElement);
     return {
-        "container": container, 
-        "image": image,
-        "index": index,
-        "name": name, 
-        "type": type, 
+        "mainContainer": mainContainer, 
+        "wrapperImage": wrapperImage,
+        "wrapperDescription": wrapperDescription,
+        "imageElement": imageElement,
+        "indexElement": indexElement,
+        "nameElement": nameElement, 
+        "typeElement": typeElement, 
     };
 }
 
 function fillCard (card, pokemon) {
-    card.image.src = pokemon.thumbnail;
-    card.index.innerHTML = `Index: ${pokemon.id}`;
-    card.name.innerHTML = pokemon.name.toUpperCase();
+    card.imageElement.src = pokemon.thumbnail;
+    card.indexElement.innerHTML = `#${pokemon.id}`;
+    card.nameElement.innerHTML = pokemon.name.toUpperCase();
     switch (pokemon.type.length) {
         case 1:
-            card.type.innerHTML = `Type: ${pokemon.type[0]}`;
+            card.typeElement.innerHTML = `Type: ${pokemon.type[0]}`;
             break;
         case 2:
-            card.type.innerHTML = `Type: ${pokemon.type[0]} ${pokemon.type[1]}`
+            card.typeElement.innerHTML = `Type: ${pokemon.type[0]} ${pokemon.type[1]}`
             break;
     };
 }
 
 function styleCard(card) {
-    card.container.style.border = "1px red solid";
-    card.container.style.width = "170px";
-    card.container.style.height = "230px";
-    card.container.style.display = "flex";
-    card.container.style.flexDirection = "column";
-    card.container.style.alignItems = "center";
+    card.mainContainer.style.border = "1px gray solid";
+    card.mainContainer.style.width = "200px";
+    card.mainContainer.style.height = "300px";
+    card.mainContainer.style.display = "flex";
+    card.mainContainer.style.flexDirection = "column";
+    card.mainContainer.style.alignItems = "center";
+    card.mainContainer.style.position = "relative";
+    card.mainContainer.style.backgroundColor = "#d9e6f2";
 
-    card.image.style.height = "50%";
-    card.image.style.maxWidth = "90%";
-    card.image.style.marginTop = "5px"
+    card.wrapperImage.style.position = "absolute";
+    card.wrapperImage.style.left = "0";
+    card.wrapperImage.style.top = "0";
+    card.wrapperImage.style.height = "65%";
+    card.wrapperImage.style.width = "100%";
+    card.wrapperImage.style.backgroundImage = "url('./media/assets/background.jpg')";
+    card.wrapperImage.style.backgroundSize = "cover"
+    card.wrapperImage.style.display = "flex";
+    card.wrapperImage.style.alignItems = "center"; 
+    card.wrapperImage.style.justifyContent = "center"; 
 
-    card.index.style.marginBottom = '0';
+    card.wrapperDescription.style.position = "absolute";
+    card.wrapperDescription.style.top = "195px";
+    card.wrapperDescription.style.width = "100%";
 
-    card.name.style.marginBottom = '0';
+    card.imageElement.style.maxWidth = "100%";
+    card.imageElement.style.maxHeight = "100%";
 
-    card.type.style.marginBottom = '0';
+    card.indexElement.style.marginTop = "0";
+    card.indexElement.style.textAlign = "center";
+
+    card.nameElement.style.marginTop = "0";
+    card.nameElement.style.textAlign = "center";
+
+    card.typeElement.style.marginTop = "0";
+    card.typeElement.style.textAlign = "center";
 }
 
 
@@ -75,5 +102,17 @@ function styleSections (sectionCards) {
     sectionCards.style.justifyContent = "center";
     sectionCards.style.gap = "1vw";
 }
+
+function hooverBright () {
+    this.style.filter = "brightness(0.7)";
+    this.style.transform = "scale(1.05)";
+    this.style.transition = "transform 0.15s ease-in-out"
+};
+
+function hooverDark () {
+    this.style.filter = "brightness(1)";
+    this.style.transform = "scale(1)";
+};
+
 
 main();
